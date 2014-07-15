14b5fc9a9f201b297f52a327fd87c69c2809830d
Kristin Ho <kh1550@nyu.edu>
2014-07-14 21:49:03 -0400

    KHo_Homework 1 (Poetic Terrain)

---
+++ Assets/bios.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bios : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<TextMesh>().text = "Die Ruinenstadt ist immer noch schön\nIch warte lange Zeit auf deine Rückkehr\nIn der Hand ein Vergissmeinnicht\nI release my soul so you feel my song";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
